828a41498a1cbfbd837308b47d4b0273bdb371fc
Andre <andrehaffner@outlook.com>
2023-08-10 14:11:38 -0700

    add SweetAlert library

--- static/index.js
+++ static/index.js
@@ -1,76 +1,93 @@
-  function getUserNumbers() {
-      const numbersStr = document.getElementById("inputNumbers").value;
-      return numbersStr.split(",").map(num => parseFloat(num));
-    }
 
-    function calculateMean(numbersList) {
-      const sum = numbersList.reduce((acc, num) => acc + num, 0);
-      return sum / numbersList.length;
-    }
 
-    function calculateMedian(numbersList) {
-      const sortedNumbers = numbersList.slice().sort((a, b) => a - b);
-      const n = sortedNumbers.length;
-      let median = null;
-      if (n % 2 === 0) {
+function getUserNumbers() {
+    const numbersStr = document.getElementById("inputNumbers").value;
+    return numbersStr.split(",").map(num => parseFloat(num));
+}
+
+function calculateMean(numbersList) {
+    const sum = numbersList.reduce((acc, num) => acc + num, 0);
+    return sum / numbersList.length;
+}
+
+
+function calculateMedian(numbersList) {
+    const sortedNumbers = numbersList.slice().sort((a, b) => a - b);
+    const n = sortedNumbers.length;
+    let median = null;
+    if (n % 2 === 0) {
         median = (sortedNumbers[n / 2 - 1] + sortedNumbers[n / 2]) / 2;
-      } else {
+    }
+    else {
         median = sortedNumbers[Math.floor(n / 2)];
-      }
-      return median;
     }
+    return median;
+}
 
-    function calculateStandardDeviation(numbersList) {
-      const mean = calculateMean(numbersList);
-      const sumSquares = numbersList.reduce((acc, num) => acc + Math.pow(num - mean, 2), 0);
-      return Math.sqrt(sumSquares / numbersList.length);
-    }
 
-    function storeStatisticsInJSON(mean, median, standardDeviation) {
-      const statistics = {
+function calculateStandardDeviation(numbersList) {
+    const mean = calculateMean(numbersList);
+    const sumSquares = numbersList.reduce((acc, num) => acc + Math.pow(num - mean, 2), 0);
+    return Math.sqrt(sumSquares / numbersList.length);
+}
+
+
+function storeStatisticsInJSON(mean, median, standardDeviation) {
+    const statistics = {
         mean: mean,
         median: median,
         standardDeviation: standardDeviation,
-      };
+    };
 
-      const statisticsJSON = JSON.stringify(statistics);
-      localStorage.setItem("statisticsData", statisticsJSON);
-    }
+    const statisticsJSON = JSON.stringify(statistics);
+    localStorage.setItem("statisticsData", statisticsJSON);
+}
 
-    function calculateStatistics() {
-      const numbers = getUserNumbers();
-      const mean = calculateMean(numbers);
-      const median = calculateMedian(numbers);
-      const standardDeviation = calculateStandardDeviation(numbers);
 
-      storeStatisticsInJSON(mean, median, standardDeviation);
+function calculateStatistics() {
+    const numbers = getUserNumbers();
+    const mean = calculateMean(numbers);
+    const median = calculateMedian(numbers);
+    const standardDeviation = calculateStandardDeviation(numbers);
 
-      const meanResult = document.getElementById("meanResult");
-      const medianResult = document.getElementById("medianResult");
-      const standardDeviationResult = document.getElementById("standardDeviationResult");
+    storeStatisticsInJSON(mean, median, standardDeviation);
 
-      meanResult.textContent = "Mean: " + mean.toFixed(2);
-      medianResult.textContent = "Median: " + median.toFixed(2);
-      standardDeviationResult.textContent = "Standard Deviation: " + standardDeviation.toFixed(2);
-      console.log(localStorage.getItem("statisticsData"))
-    }
+    const meanResult = document.getElementById("meanResult");
+    const medianResult = document.getElementById("medianResult");
+    const standardDeviationResult = document.getElementById("standardDeviationResult");
 
-    function displayStoredStatistics() {
-      const statisticsJSON = localStorage.getItem("statisticsData");
+    meanResult.textContent = "Mean: " + mean.toFixed(2);
+    medianResult.textContent = "Median: " + median.toFixed(2);
+    standardDeviationResult.textContent = "Standard Deviation: " + standardDeviation.toFixed(2);
+    console.log(localStorage.getItem("statisticsData"))
+}
 
-      if (statisticsJSON) {
+
+function displayStoredStatistics() {
+    const statisticsJSON = localStorage.getItem("statisticsData");
+    if (statisticsJSON) {
         const statistics = JSON.parse(statisticsJSON);
-
         const meanResult = document.getElementById("meanResult");
         const medianResult = document.getElementById("medianResult");
         const standardDeviationResult = document.getElementById("standardDeviationResult");
-
         meanResult.textContent = "Mean: " + statistics.mean.toFixed(2);
         medianResult.textContent = "Median: " + statistics.median.toFixed(2);
         standardDeviationResult.textContent = "Standard Deviation: " + statistics.standardDeviation.toFixed(2);
-      }
     }
+}
 
-    window.onload = function () {
-      displayStoredStatistics();
-    };
+
+const btn = document.querySelector("#myBtn");
+btn.addEventListener("click", () => {
+    Swal.fire({
+        title: "Statistics calculated!",
+        text: "Thanks for using our app.",
+        icon: "success",
+        confirmButtonText: "See results"
+    })
+})
+
+
+window.onload = function () {
+    displayStoredStatistics();
+};
